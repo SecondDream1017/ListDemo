13d70c2ef6d02233502a231877530e9433fd3444
SecondDream1017 <729351810@qq.com>
2018-09-09 16:39:14 +0800

    ArrayList去掉重复元素

---
+++ src/com/atguigu/list/ArrayListDemo.java
@@ -0,0 +1,46 @@
+package com.atguigu.list;
+
+import java.util.ArrayList;
+
+import com.atguigu.model.Student;
+
+public class ArrayListDemo {
+
+	//去掉List集合中重复的元素
+	public static void main(String[] args) {
+		ArrayList<Object> list = new ArrayList<Object>(0);
+		/*list.add("abc1");
+		list.add("abc1");
+		list.add("abc2");
+		list.add("abc2");
+		list.add("abc3");
+		list.add("abc3");*/
+		
+		list.add(new Student("aa","22"));
+		list.add(new Student("aa","22"));
+		list.add(new Student("bb","66"));
+		list.add(new Student("bb","66"));
+		
+		//方法1,用HashSet
+		/*HashSet<String> h = new HashSet<String>(list);
+		list.clear();
+		list.addAll(h);
+		System.out.println(list);*/
+		
+		//方法2,创建临时容器,在放回原List里
+		ArrayList<Object> listTemp = new ArrayList<Object>(0);
+		for (Object str : list) {
+			if(!listTemp.contains(str)){
+				listTemp.add(str);
+			}
+		}
+		list.clear();			//删除所有元素
+		list.addAll(listTemp);	//将临时容器中的元素放到原List中
+		System.out.println(list);
+		
+	}
+	
+	/*public void getElements(List list){
+		
+	}*/
+}
